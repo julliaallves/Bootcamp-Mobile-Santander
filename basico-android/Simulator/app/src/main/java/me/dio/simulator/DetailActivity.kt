7d1c7a4198115998900725dea7afd217ca8a3546
Julia Alves <julliaalves123@gmail.com>
2022-07-19 19:08:24 -0300

    Edições UI/UX

---
+++ basico-android/Simulator/app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -0,0 +1,18 @@
+package me.dio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding //pra chamar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater) //pra chamar
+        setContentView(binding.root) //pra chamar
+
+    }
+}
